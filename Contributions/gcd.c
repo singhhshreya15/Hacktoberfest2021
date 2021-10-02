7a0b45658bc60b171bd0a07da042d228f050baf9
Shreya Singh <83465477+singhhshreya15@users.noreply.github.com>
2021-10-02 20:35:37 +0530

    Create gcd.c

---
+++ Contributions/gcd.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+void main() {
+    int m, n; /* given numbers */
+    clrscr();
+    printf("Enter-two integer numbers: ");
+    scanf ("%d %d", &m, &n);
+    while (n > 0) {
+        int r = m % n;
+        m = n;
+        n = r;
+    }
+    printf ("GCD = %d \n",m);
+    getch();
+}
